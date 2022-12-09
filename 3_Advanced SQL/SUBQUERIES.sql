694bba5d4d124996fb13e5f0f23ebe6f95f0511a
Alex Babou <ababou@dendev.net>
2022-12-08 23:03:30 -0800

    Update SUBQUERIES.sql

--- 3_Advanced SQL/SUBQUERIES.sql
+++ 3_Advanced SQL/SUBQUERIES.sql
@@ -12,21 +12,27 @@ FROM EmployeeSalary -- Adds a column that includes the table's average salary at
 
 -- How to do it with PARTITION BY
 SELECT EmployeeID, Salary, AVG(Salary) OVER () AS AllAvgSalary
-FROM EmployeeSalary -- Adds a column that includes the table's average salary at the end of each row.
+FROM EmployeeSalary
 
 
 
--- Why GROUP BY doesn't work
-
-
+-- Subqueries do not work with GROUP BY
+SELECT EmployeeID, Salary, AVG(Salary) AS AllAvgSalary
+FROM EmployeeSalary
+GROUP BY EmployeeID, Salary
+ORDER BY 1,2
 
 
 -- Subquery in FROM
-
-
+SELECT Sub.EmployeeID, AllAvgSalary
+FROM (SELECT EmployeeID, Salary, AVG(Salary) OVER () AS AllAvgSalary -- This is not the most efficient, but is an example.
+	  FROM EmployeeSalary) AS Sub
 
 
 -- Subquery in WHERE
-
-
+SELECT EmployeeID, JobTitle, Salary
+FROM EmployeeSalary -- This table does not contain age information, we will use the subquery to pull that data.
+WHERE EmployeeID IN (SELECT EmployeeID -- This subquery creates a list of IDs of employees over 30.
+					 FROM EmployeeDemographics
+					 WHERE AGE > 30)
 
